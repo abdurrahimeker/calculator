ddf7485fd791289ecdba2c44deed0f9a6c861ff4
abdurrahim eker <90318672+abdurrahimeker@users.noreply.github.com>
2022-06-22 11:31:39 +0300

    Update index.js

--- deneme/index.js
+++ deneme/index.js
@@ -3,65 +3,9 @@ window.onload = function(){
     //değişkenlerimizi tanımladık
 
     var selectedOperator = "";
-    var leftpart = "";
-    var rigthpart = "";
+    var degerA = "";
+    var degerB = "";
 
-    //hesap makinesinin üzerinde bulunan tuşları klavyeye atadım
-
-    window.onkeydown = function(tus){
-        for(var i = 96; i < 105; i++){
-            
-            var b = i-48;
-            
-            if(tus.keyCode == i || tus.keyCode == b){
-
-                var a = i - 96;
-                document.getElementById("input").value += a;
-                
-                if(selectedOperator == ""){
-                    leftpart += a;
-                    
-                }else if(selectedOperator != ""){
-                    rigthpart += a;
-    
-                }
-
-            }
-    
-        }
-        var element = document.getElementById("input")
-        var operator = ""
-
-        if(tus.keyCode == 107){
-            
-            operator =  "+";
-        
-        }else if(tus.keyCode == 109){
-
-            operator =  "-";
-        
-        }else if(tus.keyCode == 106){
-
-            operator =  "*";
-
-        }else if(tus.keyCode == 111){
-
-            operator =  "/";
-
-        }else if(tus.keyCode == 13){
-
-
-        }else if(tus.keyCode == 8){
-
-
-        }else if(tus.keyCode == 27){
-            leftpart = ""
-            rigthpart = ""
-            operator =  ""
-
-        }
-        element.value += operator
-    }
     //numaralara click özelliği verdim
 
     var numbers = document.querySelectorAll(".num")
@@ -69,19 +13,20 @@ window.onload = function(){
     for(var i = 0; i < numbers.length; i++){
         numbers[i].addEventListener("click", function(){
 
-            document.getElementById("input").value += this.value;
-                    
             // console.log(this.value)
 
             if(selectedOperator == "+" || selectedOperator == "-" || selectedOperator == "/" || selectedOperator == "*"){
                 
-                let degerB = this.value
-                console.log("değerSON: "+degerB)
+                degerA = this.value
+                var inptA = document.getElementById("input").value += degerA
+                degerA =  inptA.split("+")
+                console.log(degerA)
 
             }else if(selectedOperator == ""){
-                
-                let degerA = this.value
-                console.log("degerİLK: "+degerA)
+
+                degerB = this.value
+                var inptB = document.getElementById("input").value += degerB
+                console.log(degerB)
 
             }
 
@@ -100,23 +45,25 @@ window.onload = function(){
 
             //oparatöelerin yan yana gelme sorunu
             
-            var opList = ["+","-","*","/"," "]  
+            var opList = ["+","-","*","/"]  
             var deger1 = document.getElementById("input").value
             var lastChar = deger1.substr(deger1.length-1)
             
             if (!opList.includes(lastChar)) {
                 
                 document.getElementById("input").value += this.value
-            
+                
             } else {
             
                 var newStr = deger1.substr(0, deger1.length-1) + this.value
                 document.getElementById("input").value = newStr
             
             }
+
             
             selectedOperator = this.value
 
+            
             
             
         })
@@ -124,45 +71,54 @@ window.onload = function(){
     
 
 
-    //silme butonlarına click özelliği vermek
-
-    var deleteC = document.getElementById("deleteC")
-    var deleteB = document.getElementById("deleteB")
-    
-    //tek tek silen tuş
-    deleteB.addEventListener("click", function(){
-        var input = document.getElementById("input").value
-        document.getElementById("input").value = input.slice(0,input.length-1)
-        selectedOperator = "";
-        leftpart = document.getElementById("input").value;
-        rigthpart = "";
-        
-    })
 
 
+    //işlem hane
 
-
-
-    //hepsini silen tuş
-    deleteC.addEventListener("click", function(){
-
-        document.getElementById("input").value = ""
-        selectedOperator = ""
-        leftpart = ""
-        rigthpart = ""
-        operator =  "";
-
-    })
-
-
-    //sssssssssssssssssssssssssss
     document.getElementById("esittir").addEventListener("click",function test(){
 
+        if(degerA[1] == "+"){
+
+            var sonuc =  degerA[0] + degerA[2]
+
+        }else if(degerA[1] == "-"){
+
+            sonuc =  degerA[0] - degerA[2]
+
+        }else if(degerA[1] == "*"){
+
+            sonuc =  degerA[0] * degerA[2]
+
+        }else if(degerA[1] == "/"){
+
+            sonuc =  degerA[0] / degerA[2]
+
+        }        
+
+        // ssssssssssssssssssssssssssssssssssssss
+
+        if(degerA[1] == "+" && degerA[3] == "-"){
+
+            sonuc =  degerA[0] + degerA[2] - degerA[4]
+
+        }else if(degerA[1] == "-" && degerA[3] == "-"){
+
+            sonuc =  degerA[0] - degerA[2] - degerA[4]
+
+        }else if(degerA[1] == "*" && degerA[3] == "-"){
+
+            sonuc =  degerA[0] * degerA[2] - degerA[4]
+
+        }else if(degerA[1] == "/" && degerA[3] == "-"){
+
+            sonuc =  degerA[0] / degerA[2] - degerA[4]
+
+        }
+
+
+
         selectedOperator = "";
-        leftpart = document.getElementById("input").value;
-        rigthpart = "";
 
     },false)
-
 
 }
